1bd079cecab238de67e620c82ec2091463874e3a
Christopher Speltz <chrisspeltz@msn.com>
2023-04-02 08:52:12 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <h1>Would you rather...</h1>
+
+    <would-you-rather
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed='answerChanged'
+    ></would-you-rather>
+
+    <p> {{ userSelectionMessage }}</p>
+
+  </div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data() {
+    return {
+      wyrQuestion: "Would you rather be incredibly lucky with average intelligence or incredibly smart with average luck?",
+      wyrAnswer1: 'Incredibly lucky with average intelligence',
+      wyrAnswer2: 'Incredibly smart with average luck',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice) {
+      this.userSelectionMessage = `Thanks! You chose ${choice}`
+    }
   }
 }
 </script>
